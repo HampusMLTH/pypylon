a693e54f976acdb1b7a434bf2a52542d4d37048d
HampusMLTH <55540946+HampusMLTH@users.noreply.github.com>
2020-04-21 11:14:38 +0200

    hem

---
+++ basler_controller.py
@@ -0,0 +1,161 @@
+# LoadAndSaveConfig.py
+from pypylon import pylon
+from queue import Queue
+import platform
+import time
+import threading
+import shutil
+import os
+
+#import logging, sys
+
+class BaslerController(object):
+    """ Controller class for the basler camera """
+    
+
+    def __init__(self, folder_path, queue):
+        self.queue = queue
+        self.img = pylon.PylonImage()
+        self.cam = pylon.InstantCamera(pylon.TlFactory.GetInstance().CreateFirstDevice())
+        self.nbr_im = 9
+        self.folder_path = folder_path
+        os.mkdir(self.folder_path) # unique folder for this measurement
+        self.counter = 0
+        self.thread_move = None
+        self.node_file = "daA1600-60um_gain.pfs"
+        
+        
+    def open_camera(self):
+        self.cam.Open()
+    
+    def close_camera(self):
+        self.cam.Close()
+    
+    def update_value_nodemap(self, field, new_value):
+        file = open(self.node_file, "r")
+        new_file_contents = ""
+        for line in file:
+            if field in line:
+                print(field + "updated from {} to {}".format(line.split()[1], new_value))
+                line = line.replace(line.split()[1], new_value)
+            new_file_contents += line
+        file.close()
+        file = open(self.node_file, "w")
+        file.write(new_file_contents)
+        file.close()
+        
+    def update_nodemap(self):
+        # The name of the pylon file handle
+        
+        shutil.copy(self.node_file, self.folder_path + self.node_file) # make a copy of the settings used for this measurement
+        # Print the model name of the camera.
+        print("Using device ", self.cam.GetDeviceInfo().GetModelName())
+
+        # featurePersistence = pylon.FeaturePersistence()
+
+        # read the content of the file back to the camera's node map with enabled validation.
+        print("Updating nodefile {} to camera's node map.".format(self.node_file))
+        pylon.FeaturePersistence.Load(self.node_file, self.cam.GetNodeMap(), True)
+        
+    
+    def cont_acq(self):
+        """starting a continuos exposure, stopped by setting the stop function"""
+        self._stop_cont_acq = False
+        self._save_images = False
+        self.thread_cont = threading.Thread(target=self._cont_acq,
+                                            args=(lambda : self._stop_cont_acq,
+                                                  lambda : self._save_images,
+                                                  lambda : self._folder_name,))
+        self.thread_cont.start()
+        
+    def stop_cont_acq(self):
+        self._stop_cont_acq = True
+        print("stop sent")
+        
+        self.thread_cont.join()
+    
+    def save_images(self, folder_name):
+        self._save_images = True
+        self._folder_name = folder_name
+        print("save sent")
+    
+    def _cont_acq(self, stop, save_images, folder_name):
+        self.cam.StartGrabbing()
+        t_grab = time.time()
+        nbr_imgs_saved = 0
+        while not stop():
+                
+            with self.cam.RetrieveResult(2000) as result:
+                self.counter = self.counter + 1
+                
+                print("putting")
+                self.queue.put(result.Array)                
+                print("bc queue size: {}".format(self.queue.qsize()))
+                # Calling AttachGrabResultBuffer creates another reference to the
+                # grab result buffer. This prevents the buffer's reuse for grabbing.
+                
+                #import pdb;pdb.set_trace()
+                #self.img.AttachGrabResultBuffer(result)
+
+                # In order to make it possible to reuse the grab result for grabbing
+                # again, we have to release the image (effectively emptying the
+                # image object).
+               
+                if save_images():
+                    print("save images")
+                    self.img.AttachGrabResultBuffer(result)
+                    nbr_imgs_saved += 1
+                    filename = "%d.tiff" % (self.counter % 9)
+                    self.img.Save(pylon.ImageFileFormat_Tiff , filename)
+                    if nbr_imgs_saved == self.nbr_im:
+                        self._save_images = False
+                        #move images
+                        #check if previous move images is done, otherwise throw error
+                        if self.thread_move:
+                            if self.thread_move.isAlive():
+                                raise BufferError("last batch of are currently being moved, you should lower frame rate or get a faster harddrive.")
+                        self.thread_move = threading.Thread(target=self._move_images, args=(folder_name(),))
+                        self.thread_move.start()
+                
+                #if self.thread_move:
+                 #   if self.thread_move.isAlive():
+                  #      print("Moving thread active!")
+                   # else:
+                        #print("not active")
+                #print(self.counter % 9, end = ',')
+                
+                #self.img.Release()
+                #if i % 10 == 0:
+                    #print(i)
+                    #print("counter at %d" % self.counter)
+                    #print("mod %d" % (self.counter % 9))
+                    # Printing every 10 exposures
+        self.cam.StopGrabbing()
+        print("Continuous acquisition stopped after", time.time() - t_grab, "seconds")
+    
+    
+    
+    
+    def _move_images(self, folder_name):
+        t_move = time.time()
+        #timestr = time.strftime("%Y%m%d-%H%M%S/")
+        #coord_str = "led_{}_stage_{}_sample_{}/".format(coords[0], coords[1], coords[2])
+        os.mkdir(self.folder_path  + folder_name)
+        for i in range(self.nbr_im):
+            print("moving {}".format(i))
+            
+            #shutil.move("{}.tiff".format((self.counter + i) % 9), self.folder_path  + coord_str + "{}.tiff".format((self.counter + i) % 9))
+            shutil.move("{}.tiff".format(i), self.folder_path  + folder_name + "/{}.tiff".format(i))
+        print("time of moving 9 images: ", time.time() - t_move)
+    
+    def move_images(self, coords):
+        t_move = time.time()
+        #timestr = time.strftime("%Y%m%d-%H%M%S/")
+        coord_str = "led_{}_stage_{}_sample_{}/".format(coords[0], coords[1], coords[2])
+        os.mkdir(self.folder_path  + coord_str)
+        for i in range(self.nbr_im):
+            print("moving {}".format((self.counter + i) % 9))
+            print("counter is {}".format(self.counter))
+            shutil.move("{}.tiff".format((self.counter + i) % 9), self.folder_path  + coord_str + "{}.tiff".format((self.counter + i) % 9))
+        print("time of moving 9 images: ", time.time() - t_move)
+        
---
+++ tkinter_plot.py
@@ -0,0 +1,418 @@
+# The code for changing pages was derived from: http://stackoverflow.com/questions/7546050/switch-between-two-frames-in-tkinter
+# License: http://creativecommons.org/licenses/by-sa/3.0/	
+
+
+# TODO: browse folder to initiate bc
+#move variables to bc such as dynamic range
+import matplotlib
+matplotlib.use("TkAgg")
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
+from matplotlib.figure import Figure
+import matplotlib.animation as animation
+from matplotlib import style
+
+import tkinter as tk
+from tkinter import ttk
+
+import time
+from basler_controller import BaslerController
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+
+from queue import Queue
+from queue import Empty
+import threading
+
+MAX_QSIZE = 10
+LED_WAVELENGTHS = ["365 nm",
+                   "405",
+                   "430",
+                   "490",
+                   "525",
+                   "630",
+                   "810",
+                   "940",]
+LARGE_FONT= ("Verdana", 12)
+style.use("ggplot")
+
+f = Figure(figsize=(5,5), dpi=100)
+fig_image = f.add_subplot(211)
+fig_hist = f.add_subplot(212)
+#folder_path = "/home/pi/Desktop/BrickPi3-master/Software/Python/Testing Scripts/pypylon/images/" 
+#folder_path = "/" + time.strftime("%Y%m%d-%H%M%S/")
+folder_path = "sample_imgs/" + time.strftime("%Y%m%d-%H%M%S/")
+q = Queue(maxsize=MAX_QSIZE)
+bc = BaslerController(folder_path, q)
+#bc = BaslerController(folder_path)
+
+
+
+
+
+
+    
+
+def animate(i):
+    pass
+    
+            
+
+class GoniometerApp(tk.Tk):
+
+    def __init__(self, *args, **kwargs):
+        
+
+        self.nbr_exposures = 9
+        self.led_background = -1
+        tk.Tk.__init__(self, *args, **kwargs)
+
+        tk.Tk.iconbitmap(self, default="clienticon.ico")
+        tk.Tk.wm_title(self, "Goniometer")
+        
+        
+        container = tk.Frame(self)
+        container.pack(side="top", fill="both", expand = True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+        self.frames = {}
+
+        for F in (StartPage, ExposurePage, WhiteRefPage, MotorPage, MeasurementPage):
+
+            frame = F(container, self)
+
+            self.frames[F] = frame
+
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame(StartPage)
+
+    def show_frame(self, cont):
+
+        frame = self.frames[cont]
+        frame.tkraise()
+
+        
+class StartPage(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self,parent)
+        label = tk.Label(self, text="Start Page", font=LARGE_FONT)
+        label.pack(pady=10,padx=10)
+
+      
+
+       
+
+        button1 = ttk.Button(self, text="1. Set exposure time",
+                            command=lambda: controller.show_frame(ExposurePage))
+        button1.pack()
+        
+        button2 = ttk.Button(self, text="2. Calibrate to white reference",
+                            command=lambda: controller.show_frame(WhiteRefPage))
+        button2.pack()
+        
+        button3 = ttk.Button(self, text="3. Calibrate goniometer motors",
+                            command=lambda: controller.show_frame(MotorPage))
+        button3.pack()
+        
+        butto4 = ttk.Button(self, text="4. Start measurement",
+                            command=lambda: controller.show_frame(MeasurementPage))
+        butto4.pack()
+
+
+class MeasurementPage(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="Page One!!!", font=LARGE_FONT)
+        label.pack(pady=10,padx=10)
+
+        button1 = ttk.Button(self, text="Back to Home",
+                            command=lambda: controller.show_frame(StartPage))
+        button1.pack()
+
+        button2 = ttk.Button(self, text="Page Two",
+                            command=lambda: controller.show_frame(MotorPage))
+        button2.pack()
+
+class WhiteRefPage(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        self.canvas = FigureCanvasTkAgg(f, self)
+        self.color_img = None
+        self.calib_val_red = 1
+        self.calib_val_green = 1
+        self.calib_val_blue = 1
+        
+        
+        label = tk.Label(self, text="WhiteRefPage!", font=LARGE_FONT)
+        label.pack(pady=10,padx=10)
+
+        button1 = ttk.Button(self, text="Back to Home",
+                            command=lambda: controller.show_frame(StartPage))
+        button1.pack()
+        button2 = ttk.Button(self, text="Update color image",
+                            command=lambda: self.show_color_image())
+        button2.pack()
+        button3 = ttk.Button(self, text="Chose calibration area",
+                            command=lambda: self.get_area())
+        button3.pack()
+
+
+        
+        self.canvas.draw()
+        self.canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
+
+        toolbar = NavigationToolbar2Tk(self.canvas, self)
+        toolbar.update()
+        self.canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+
+    def get_area(self):
+        plt.imshow(self.color_img)
+        plt.show(block=False)
+        reference_coords = plt.ginput(2)
+        self.x_start = int(round(reference_coords[0][0]))
+        self.x_end = int(round(reference_coords[1][0]))
+        self.y_start = int(round(reference_coords[0][1]))
+        self.y_end = int(round(reference_coords[1][1]))
+        print(reference_coords)
+
+    def calibrate_area(self):
+        white_ref = self.color_img[self.x_start:self.x_end, self.y_start, self.y_end,:]
+        self.calib_val_red = white_ref[:,:,0].mean()
+        self.calib_val_green = white_ref[:,:,1].mean()
+        self.calib_val_blue = white_ref[:,:,2].mean()
+        print(self.calib_val_red)
+        print(self.calib_val_green)
+        print(self.calib_val_blue)
+        
+        self.color_img[:,:,0] = self.color_img[:,:,0] / self.calib_val_red
+        self.color_img[:,:,1] = self.color_img[:,:,1] / self.calib_val_green
+        self.color_img[:,:,2] = self.color_img[:,:,2] / self.calib_val_blue
+        self.color_img = self.color_img/np.max(self.color_img)
+
+   
+
+
+
+
+class MotorPage(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="Page Two!!!", font=LARGE_FONT)
+        label.pack(pady=10,padx=10)
+
+        button1 = ttk.Button(self, text="Back to Home",
+                            command=lambda: controller.show_frame(StartPage))
+        button1.pack()
+
+        button2 = ttk.Button(self, text="Page One",
+                            command=lambda: controller.show_frame(MeasurementPage))
+        button2.pack()
+
+
+class ExposurePage(tk.Frame):
+    class_canvas = None
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+
+        self.controller = controller
+        ExposurePage.class_canvas = FigureCanvasTkAgg(f, self)
+
+
+        
+        label = tk.Label(self, text="Graph Page!", font=LARGE_FONT)
+        label.pack(pady=10,padx=10)
+
+        button1 = ttk.Button(self, text="draw",
+                            command=lambda: self.draw())
+        button1.pack()
+        
+        
+        button2 = ttk.Button(self, text="update graph",
+                            command=lambda: self.update_graph())
+        button2.pack()
+
+        label_exp_time = tk.Label(self, text="enter exposure time below")
+        label_exp_time.pack()
+        e = tk.Entry(self)
+        e.pack()
+        #e.delete(0, END)
+        e.insert(0, "Exposure time")
+        
+        button3 = ttk.Button(self, text="update exposure time",
+                            command=lambda: self.update_exposure_time(e.get()))
+        button3.pack()
+        
+        button4 = ttk.Button(self, text="close camera",
+                            command=lambda: self.close_camera())
+        button4.pack()
+         
+        #button5 = ttk.Button(self, text="start live",
+        #                    command=lambda: self.start_live_view())
+        #button5.pack()
+        
+        #button6 = ttk.Button(self, text="stop live",
+        #                    command=lambda: self.stop_live_view())
+        #button6.pack()
+        
+        self.red_LED = ttk.Combobox(self, 
+                            values=LED_WAVELENGTHS, state="readonly")
+        self.green_LED = ttk.Combobox(self, 
+                            values=LED_WAVELENGTHS, state="readonly")
+        self.blue_LED = ttk.Combobox(self, 
+                            values=LED_WAVELENGTHS, state="readonly")
+        
+        self.red_LED.pack()
+        self.red_LED.current(2)
+        self.green_LED.pack()
+        self.green_LED.current(4)
+        self.blue_LED.pack()
+        self.blue_LED.current(5)
+        
+
+
+       
+        ExposurePage.class_canvas.draw()
+        ExposurePage.class_canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
+
+        toolbar = NavigationToolbar2Tk(ExposurePage.class_canvas, self)
+        toolbar.update()
+        ExposurePage.class_canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+    
+    def draw(i):
+        ExposurePage.class_canvas.draw()
+        print("redrawing")
+            
+    
+    def close_camera(self):
+        self.stop_live_view()
+        bc.stop_cont_acq()
+        bc.close_camera()
+    
+    def consumer_thread(self, stop):
+        #
+        print("in thread")
+        while not stop():
+            
+            print("----------stop is {}".format(stop()))
+            i = 0
+            images = []
+            while i < 9:
+
+                #img = plt.imread("sample_imgs/{}.tiff".format(i))
+
+                try:
+                    img = q.get(timeout=1)
+                except Empty:
+                    print("timeout reached, i is {}".format(i))
+                    if stop():
+                        break
+                else:
+                    #
+                    images.append(img)
+                    q.task_done()
+                    
+                    i += 1
+             
+            
+           
+            
+            #self.canvas.draw()
+            print("show images now")
+            self.show_color_image(images)
+        print("bottom")
+            
+
+    def show_color_image(self, images):
+        #add option of chosing red green blue leds
+        print("show colorim")
+        print("red is {} green is {} blue is {}".format(self.red_LED.get(), 
+                                                        self.green_LED.get(), 
+                                                        self.blue_LED.get(),))
+        #self.comboExample.current()
+        
+        dynamic_range = 4095#65520
+        darkest_img_mean = sys.maxsize
+        index_background = -1
+        i = 0
+        fig_hist.clear()
+        for image in images:
+            img_mean = image.mean()
+            fig_hist.hist(image.flatten(), 32, label='LED {}'.format(i), alpha=0.5)
+            print("LED {} has a mean off: {}".format(i, img_mean))
+            if img_mean < darkest_img_mean:
+                darkest_img_mean = img_mean
+                index_background = i
+            i += 1
+        fig_hist.legend(loc='upper right')
+        self.controller.led_background = index_background
+        #todo fix what colors
+        img_off =images[0]
+        img_r =images[6]
+        img_g =images[5]
+        img_b =images[3]
+        #img_r = plt.imread("sample_imgs/{}.tiff".format((self.controller.led_background + 6) % 9))
+        #img_g = plt.imread("sample_imgs/{}.tiff".format((self.controller.led_background + 5) % 9))
+        #img_b = plt.imread("sample_imgs/{}.tiff".format((self.controller.led_background + 3) % 9))
+        self.color_img = np.ndarray(shape=(img_r.shape + (3,)),dtype=float)
+        red = (img_r).astype(float)
+        red = red/dynamic_range
+        green = (img_g).astype(float)
+        green = green/dynamic_range
+        blue = (img_b).astype(float)
+        blue = blue/dynamic_range
+        self.color_img[:,:,0] = red
+        self.color_img[:,:,1] = green
+        self.color_img[:,:,2] = blue
+        fig_image.clear()
+        fig_image.imshow(self.color_img)
+        #ExposurePage.draw(0)
+       
+    
+    def update_exposure_time(self, exp_time):
+        print("new exp time: " + exp_time)
+        bc.update_value_nodemap("ExposureTimeRaw", exp_time)
+        #nodemap etc
+    
+    def start_live_view(self):
+        self.stop_threads = False
+        self.cons_thread = threading.Thread(target=self.consumer_thread, args =(lambda : self.stop_threads, ))
+        self.cons_thread.start()
+    
+    def stop_live_view(self):
+        self.stop_threads = True
+        #self.cons_thread.join()
+        
+    def update_graph(self):
+        
+        bc.open_camera()
+        bc.update_nodemap()
+        bc.cont_acq()
+        stop_threads = False
+        #consumer_thread = threading.Thread(target=self.consumer_thread, args =(lambda : stop_threads, ))
+        #consumer_thread.start()
+        print("thread started")
+        self.start_live_view()
+        #time.sleep(1)
+        #stop_threads = True
+        #consumer_thread.join()
+        
+        
+
+
+    def help_method(self):
+        self.controller.led_background = 0
+        print(self.controller.led_background)
+
+
+app = GoniometerApp()
+#ani = animation.FuncAnimation(f, animate, interval=2000)
+ani = animation.FuncAnimation(f, ExposurePage.draw, interval=2000)
+app.mainloop()
+        
